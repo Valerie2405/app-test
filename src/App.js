b06e5b7931f78d7810c24c46f9be7f5f168fb757
Valerie2405 <valerie.arobert@gmail.com>
2021-06-04 16:13:00 -0500

    Search Function
    - Added the environment example file where user can add their api key
    - edited the search function so now when the user types in the search box what they type appears in the console, BUT it doesn't print out the last letter :-1:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import Search from './components/Search'
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [state, setState] = useState ( {
+    s:'', // search query
+    results:[], 
+    selected: {}
+  } );
+
+  // api url
+  const apiurl = '`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchParam}&r=json`'
+
+
+//handle event: preserve what user types in search box
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    //set to previous state to not over run the other state
+    setState(prevState => {
+      return {...prevState, s:s} // getting previous state to the current s
+    });
+    
+    console.log(state.s);
+  }
   /*
   main has three components:
   Search Bar component, Result Component, Popup Component (details) 
@@ -12,7 +34,7 @@ function App() {
        <h1> Let's find you're favorite Movie !</h1> 
       </header>
       <main> 
-        <Search />
+        <Search handleInput={handleInput} />
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function Search() {
+function Search({ handleInput }) {
     return(
         <section className="searchbox-wrap">
-            <input type="text" placeholder="Search for your favorite movie here..." className="searchbox" />
+            <input 
+            type="text" 
+            placeholder="Search for your favorite movie here..." 
+            className="searchbox" 
+            //handles the input:
+            onChange={handleInput} />
         </section>
     )
 }
