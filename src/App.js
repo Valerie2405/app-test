7bf7a36c838f2e6c615b5e0346ed08729e619a6f
Valerie2405 <valerie.arobert@gmail.com>
2021-06-04 00:33:15 -0500

    Search-Component
    added the search component to the code
    Need to finalize the focus aspect of the search in index.css file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from './components/Search'
+import React from 'react';
 
 function App() {
+  /*
+  main has three components:
+  Search Bar component, Result Component, Popup Component (details) 
+  */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+       <h1> Let's find you're favorite Movie !</h1> 
       </header>
+      <main> 
+        <Search />
+      </main>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Search() {
+    return(
+        <section className="searchbox-wrap">
+            <input type="text" placeholder="Search for your favorite movie here..." className="searchbox" />
+        </section>
+    )
+}
+export default Search;
