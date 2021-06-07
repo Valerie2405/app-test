3c77cb932a8595f5fd4fec48a988dcca4d55efa3
Valerie2405 <valerie.arobert@gmail.com>
2021-06-07 13:51:46 -0500

    Update Popup
    So when a movie is clicked on the result is appearing however it is appearing at the bottom
    Reformatted the way the information is appears on the screen

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,14 +4,14 @@ function Popup({selected,closedPopup}){
     return(
         <section className="popup">
             <div className="information">
-                <h2>{selected.Title}
-                <span>{selected.Year}</span>
-                <span>{selected.Director}</span>
-                </h2>
-                <p className="release date">Release Date:{selected.Released}</p>
+                <h2>{selected.Title} </h2>
+                <p className="year"> <h3>Year: {selected.Year}</h3> </p>
+                <p className="director"> <h5>Director: {selected.Director}</h5> </p>                
+                <p className="release date"> <h5>Release Date: {selected.Released}</h5></p>
+                <p className="genre"> <h5>Genre: {selected.Genre}</h5></p>
+
                 <div className="poster">
                     <img src={selected.Poster} />
-                    <p>{selected.Genre}</p>
                 </div>
                 <button className="exit" onClick={closedPopup}>Close</button>
             </div>
